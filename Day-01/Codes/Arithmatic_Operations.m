7f0cf95f50c93a0de8b5620f19d0904d0559ecbc
Sajidha-Nasakath <sajidhanasahath@gmail.com>
2025-05-28 18:58:50 +0530

    Created Day-01 Folder

---
+++ Day-01/Codes/Arithmatic_Operations.m
@@ -0,0 +1,49 @@
+%Arithmetic operations
+
+a = 5
+b = 3
+a+b
+a-b
+a*b
+a/b
+(a+b)*(a-b)
+c = a + b
+
+%Arrays
+
+A = [2,3,4]
+B = [3;5;6]
+c =[2 3 4;5 6 7;8 9 10]
+
+size(c)
+numel(c)
+sum(c,1)
+sum(c,2)
+sum(sum(c))
+min(c)
+min(min(c))
+max(c)
+max(max(c))
+c(2)
+c(2,2)
+c(1,:)
+c(:,2)
+c(1,end)
+c(end,2)
+A=[1,0,0,0;0,1,0,0;0,0,1,0;0,0,0,1]
+
+B=[0,0,0,1;0,0,1,0;0,1,0,0;1,0,0,0]
+
+A+B
+
+A-B
+
+A*B
+A+5
+A-2
+A*2
+A/2
+A.*B
+C = ones(4,3)
+[A B]
+[A;B]
---
+++ Day-01/Codes/First_Program.m
@@ -0,0 +1,7 @@
+num = -10;
+
+if num>0
+    disp('The value is positive!')
+else
+    disp('The value is negative!')
+end
